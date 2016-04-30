7aa8ebceabb6224e2b3fdacdec84655109a2c4a1
kishoretvk <kishoretvk@hotmail.com>
2016-04-30 18:31:29 -0400

    this is the first commit hopping to be successful, with out bugs

---
+++ RealEstate/RealEstate/App_Start/RealEstateContext.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using RealEstate.Properties;
+
+namespace RealEstate
+{
+    public class RealEstateContext
+    {
+        public MongoDatabase Database;
+
+        public RealEstateContext()
+        {
+            var client = new MongoClient(Settings.Default.RealEstateConnectionString);
+
+            var server = client.GetServer();
+
+            Database = server.GetDatabase(Settings.Default.RealEstateDatabaseName);
+        }
+    }
+}
